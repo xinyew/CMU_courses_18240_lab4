1a6bc226c12339d302f9c2747dddb44b796132d3
Xinye Wang <xinyew@hh-1305-26.andrew.local.cmu.edu>
2023-03-28 16:52:47 -0400

    file renamed

--- csrc/filelist.cu
+++ csrc/filelist.cu
@@ -4,16 +4,16 @@ ifeq ($(LDVERSION), 0)
 endif
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _11104_archive_1.so
-_11104_archive_1.so : archive.34/_11104_archive_1.a
+ARCHIVE_OBJS += _1986_archive_1.so
+_1986_archive_1.so : archive.35/_1986_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_11104_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_1986_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_11104_archive_1.so $@
+	@ln -sf .//../simv.daidir//_1986_archive_1.so $@
 
 
 ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.34/_prev_archive_1.a
+_prev_archive_1.so : archive.35/_prev_archive_1.a
 	@$(AR) -s $<
 	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
--- parser.py
+++
@@ -1,5 +0,0 @@
-with open('./lab5_task1.vm', 'r') as f:
-    string = f.read()
-    for i in range(len(string)//16):
-        print('----------' + str(i) + '--------')
-        print(string[i*16:i*16+16])
